e1de265c32ada4bf5660750c04439f5070a36be4
David Rogers <al.the.x@gmail.com>
2015-06-24 11:54:55 -0400

    Refactoring to `ng-include` for "pages"...

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,21 @@
 (function(window) {
-  var app = angular.module('repos', []);
+  // With jQuery.load...
+  // $('main.container').load('views/repositories.html');
 
+  var app = angular.module('tiy-gradebook', [ ]);
 
-  app.controller('Repos.Controller', function() {
+  app.controller('MainController', function(){
+    this.page = function(name){
+      this.view = 'views/404.html';
 
+      if ( name == 'milestones' ){
+        this.view = 'views/milestones.html';
+      }
+      if ( name == 'repositories' ){
+        this.view = 'views/repositories.html';
+      }
+    }
 
-
-
-  });
-
-
-
-
-
+    this.page('repositories');
+  }); // END controller(MainController)
 })(window);
