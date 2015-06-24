474202f2d71ab2e52a90bec0b8ec9493b0eae80c
Sean Joyal <sean.joyal@gmail.com>
2015-06-24 11:54:33 -0400

    wired up some html to remove duplication, link page views together

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+(function(window){
+  // with jQuery.load
+  // $('main.container').load('views/repositories.html');
+
+  var app = angular.module('tiy-gradebook', [ ]);
+
+  app.controller('MainController', function(){
+    this.view = null;
+    this.page = function(name){
+      this.view = 'view/404.html';
+      if ( name == 'milestones' ){
+        this.view = 'views/milestones.html';
+      }
+      if ( name == 'repositories' ){
+        this.view = 'views/repositories.html';
+      }
+    };
+  }); // END controller(MainController)
+
+})(window);
